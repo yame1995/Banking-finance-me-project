a447320a18b27412734a6847632ab2db62722416
fatima <110208921+Fatima-Ishrath021@users.noreply.github.com>
2023-04-25 18:17:13 +0530

    edited access key

--- my-serverfiles/provider.tf
+++ my-serverfiles/provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
 region     = "ap-south-1" 
-  access_key = "AKIA6A5CSFGYXJ5W53EU"
-  secret_key = "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"
+  access_key = "AKIA6A5CSFGYQN4CXWUY"
+  secret_key = "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"
 }
